73c694067bccbf0e8313d3c7b7c2a5a585cbad87
gilbertot272 <84149333+gilbertot272@users.noreply.github.com>
2022-05-06 13:13:08 -0500

    Add files via upload

--- scri.js
+++ scri.js
@@ -17,17 +17,43 @@ function pdfexport(){
 
 
 window.jsPDF = window.jspdf.jsPDF;
- //var doc = new jsPDF();
- var doc = new jsPDF();
+ 
+// var doc = new jsPDF();
+ 
+var doc = new jsPDF();
  doc.setFontSize(18);
- doc.text(ltitle,60, 10);
- doc.setFontSize(16);
- doc.text(nombrev+ ''  + inom, 10,20);
- doc.text(edadv + ''   + ieda, 10, 30);
- doc.text(fechav + ''  + ifech, 10, 40);
- doc.text(desv + ''    + ides, 10, 50);
 
- doc.save('1.pdf');
- //doc.output('dataurlnewwindow',{A1: ltitle.value});
+ doc.text(ltitle,60, 10);
+ doc.setFontSize(14);
+ 
+ doc.text(80,20,"ISP SOFT-MEX SA de CV.");
+ 
+ 
+var columns = ["DATOS DEL REPORTE"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t"+fechav.toUpperCase()+ifech];
+var data = [
+[nombrev.toUpperCase()+ ''  + inom.toUpperCase()],
+[edadv.toUpperCase() + ''   + ieda.toUpperCase()],
+[desv.toUpperCase() + '' + "\n" + "\n"   + ides.toUpperCase()] ];
+//var columns = ["Nombre", "Edad", "Falla"];
+doc.autoTable(columns,data,
+{ margin:{ top: 25 }}
+);
+
+doc.save('1.pdf');
+
+ //doc.setFontSize(18);
+
+ //doc.text(ltitle,60, 10);
+ //doc.setFontSize(14);
+ 
+ //doc.text(fechav  + ''  + ifech, 110, 20);
+ //doc.text(nombrev+ ''  + inom, 10,30);
+ //doc.text(edadv + ''   + ieda, 10, 40);
+
+ //doc.text(desv + ''    + ides, 10, 50);
+
+ //pdf.save('1.pdf');
+ 
 
  }
+ 
