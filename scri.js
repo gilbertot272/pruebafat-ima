691edb7cf3ab5e1aaecd044347f551a3639f3875
gilbertot272 <84149333+gilbertot272@users.noreply.github.com>
2022-05-22 15:41:18 -0500

    Add files via upload

--- scri.js
+++ scri.js
@@ -123,7 +123,7 @@ margin:{ top: 40
 var imgWidth = 75;
 var imgHeight = 90;
 
-doc.addImage(imgdata, 70, 125, 62, 69, 0, 0, -90);
+doc.addImage(imgdata, 70, 125, 60, 69, 0, 0, -90);
 
 
 //ancho,entrado,mediddas
